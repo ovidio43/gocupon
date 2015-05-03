89450625b6ac3e6b92a027305794b6ce39d4fb7a
Edwin Condori <ovidio43@gmail.com>
2015-05-03 11:45:22 -0400

    add

--- wp-content/themes/themeGocupon/woocommerce/myaccount/form-login.php
+++ wp-content/themes/themeGocupon/woocommerce/myaccount/form-login.php
@@ -97,7 +97,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 			<?php do_action( 'woocommerce_register_form' ); ?>
 			<?php do_action( 'register_form' ); ?>
-
+			<br style="clear:both;">Al hacer click en "Registrar", acepto que:
+			<ul>
+				<li style="margin: 0 0 0 18px;padding:0;list-style:initial;">He leido y aceptado las <a href="/terminos-y-condiciones/">condiciones de uso</a> y el <a href="/privacidad/">aviso de privacidad</a>.</li>
+			</ul>
+			 
 			<p class="form-row">
 				<?php wp_nonce_field( 'woocommerce-register' ); ?>
 				<input type="submit" class="button" name="register" value="<?php _e( 'Register', 'woocommerce' ); ?>" />
